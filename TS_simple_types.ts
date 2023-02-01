0a6a99cbb14f3e02079961193a736eb15f07f273
Daniel Williams <williamsoligodaniel@gmail.com>
2023-02-01 13:00:25 +0100

    Create TS_simple_types.ts

---
+++ TS_simple_types.ts
@@ -0,0 +1,29 @@
+/*
+TypeScript Simple Types
+TypeScript supports some simple types (primitives) you may know.
+
+There are three main primitives in JavaScript and TypeScript.
+
+boolean - true or false values
+number - whole numbers and floating point values
+string - text values like "TypeScript Rocks"
+Type Assignment
+When creating a variable, there are two main ways TypeScript assigns a type:
+
+Explicit
+Implicit
+In both examples below firstName is of type string
+
+Explicit Type
+Explicit - writing out the type:
+
+let firstName: string = "Dylan";
+Explicit type assignment are easier to read and more intentional. */
+
+let firstName: string = 'Dan';
+console.log(firstName)
+
+/*
+Implicit Type
+Implicit - TypeScript will "guess" the type, based on the assigned value:
+*/
